d05449d75ae0547d3e1ba54b407a1f7e478f3052
Al3xand3r11 <alexander.nance11@gmail.com>
2024-12-03 22:33:25 -0800

    nav and view

---
+++ app/components/navbar.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Navbar () {
+    return (
+        <>
+            <div className="h-20 w-full sticky top-0">
+                <div className="container mx-12 h-full">
+                    <div className="flex justify-between items-center h-full text-white">
+                        <div>
+                        <Image
+                            src="/SDRDnav.png"
+                            alt="SundayRunday"
+                            width={150}
+                            height={150}
+                        />
+                        </div>
+                        <div>
+                            <Link href="/">
+                                Home
+                            </Link>
+                        </div>
+                        <div>
+                            Contact Us
+                        </div>
+                        
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
